bf56c41557d0a2b4ef511f84809c9e200cabb433
Tiago Garcia <thiaggoggarcia@gmail.com>
2025-10-23 18:32:33 -0300

    feat: Adicionando funcionalidade para envio de músicas via formulário e integrar a uma Playlist no Spotify

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request, jsonify
+from dotenv import load_dotenv
+import os
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+SCOPE = os.getenv("SCOPE")
+PLAYLIST_ID = os.getenv("PLAYLIST_ID")
+
+# Inicializa o cliente Spotify
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
+                                               client_secret=CLIENT_SECRET,
+                                               redirect_uri=REDIRECT_URI,
+                                               scope=SCOPE
+                                               ))
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return render_template('index.html')
+
+#TODO: Implementar via JavaScript fetch para não recarregar a página
+@app.route('/music', methods=['POST'])
+def add_music():
+    music = request.form.get('message-text')
+
+    return f"<h1>Musica recebida: {music}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
